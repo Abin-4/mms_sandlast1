0dfa7426adab5a12059ca99592f70faab04411f8
Abin-4 <161850237+Abin-4@users.noreply.github.com>
2025-07-29 11:56:16 +0530

    Update serverUrl.js

--- src/Server/serverUrl.js
+++ src/Server/serverUrl.js
@@ -1,4 +1,4 @@
-export const SERVER_URL= "https://backendderversandlast1.onrender.com"
+export const SERVER_URL= "https://sand-mms-backend.onrender.com"
 
 // export const SERVER_URL= "http://localhost:3005"
  
